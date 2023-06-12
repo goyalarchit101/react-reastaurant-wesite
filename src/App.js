02167300a374aa78b9dd1ccd3864c5ab31a08cc3
Archit Goyal <goyalarchit101>
2023-06-12 21:00:36 +0530

    restaurant complete code pushed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Food from "./components/basics/Food";
+
+
+function App() {
+  return (
+    <div>
+        <Food/>
+    </div>  
+  );
+}
+
+export default App;
---
+++ src/components/basics/Food.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import "./style.css"
+import Menu from './menuApi'
+import MenuCard from './menuCard.js';
+
+const Food = () => {
+    var categoryData = [...new Set(Menu.map((currenEle) => {
+        return currenEle.category ;
+    })), "All"];
+    const [menuData, setMenuData] = useState(Menu);
+    const [category, setCategoryData] = useState(categoryData);
+    const filerCategory = (event) => {
+        if (event.target.value.toLocaleLowerCase() === String("All").toLocaleLowerCase()) {
+            setMenuData(Menu);
+        }
+        else {
+            const updatedList = (Menu.filter(menuItem => menuItem.category.toLocaleLowerCase() === event.target.value.toLocaleLowerCase()));
+            setMenuData(updatedList);
+        }
+    }
+
+    return (
+        <div>
+            <nav className='navbar '>
+                <div className='btn-group'>
+                    {category.map((currentCtgry, index) => {
+                        return (
+                            <button key={index} onClick={filerCategory} value={currentCtgry} className='btn-group__item'>{currentCtgry}</button>
+                        )
+                    })}
+                </div>
+            </nav>
+            <MenuCard menuItem={menuData} />
+        </div>
+    )
+}
+
+export default Food;
